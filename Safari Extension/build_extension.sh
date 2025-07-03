3577c96a39cb643487a18c1b4b111b4b746b580a
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2025-07-03 22:04:08 +0200

    add link to extension repository, update build script

--- Safari Extension/build_extension.sh
+++ Safari Extension/build_extension.sh
@@ -3,3 +3,4 @@ npm install --prefix Safari\ Extension/Resources/Angular
 npm run build:safari --prefix Safari\ Extension/Resources/Angular
 rm -f -R Safari\ Extension/Resources/src
 mv Safari\ Extension/Resources/Angular/dist/email-alias/browser Safari\ Extension/Resources/src
+cp -r Safari\ Extension/Resources/extension/shared/* Safari\ Extension/Resources

84610ec925052ff4de6281c8006425572d94cc61
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2025-07-14 20:35:38 +0200

    remove extension from src path

--- Safari Extension/build_extension.sh
+++ Safari Extension/build_extension.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install --prefix Safari\ Extension/Resources/extension/Angular
 npm run build:safari --prefix Safari\ Extension/Resources/extension/Angular
-rm -f -R Safari\ Extension/Resources/extension/src
+rm -f -R Safari\ Extension/Resources/src
 mv Safari\ Extension/Resources/extension/Angular/dist/email-alias/browser Safari\ Extension/Resources/src
 cp -r Safari\ Extension/Resources/extension/shared/content.js Safari\ Extension/Resources
